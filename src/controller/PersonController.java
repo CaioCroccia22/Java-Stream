6b6a5c6787b46c1468b3fa9d019276812e15718f
caiocroccia <caiocroccia@fedora>
2026-03-18 16:59:30 -0300

    fix: correct PersonController, change type of instance personDao

--- src/controller/PersonController.java
+++ src/controller/PersonController.java
@@ -3,24 +3,30 @@ package controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-import dao.PersonDAO;
+import dao.IObjectDAO;
 import model.Person;
 
 public class PersonController {
-	public PersonDAO personDao = new PersonDAO();
+	private final IObjectDAO<Person> personDao;
 		
 	public PersonController() {
+		this.personDao = null;
 		
 	}
 	
 	public void addToList(String name, char gender) {
-		Person person = personDao.createPerson(name, gender);
-		personDao.addObjectList(person);
+		try {
+			Person person = personDao.createObject(new Person(name, gender));
+			personDao.addObjectList(person);
+			
+		} catch(Exception e) {
+			System.out.println("Erro ao instanciar pessoa");
+		}
+		
 	}
 	
 	public List<Person> filterListPeople(){
-		List<Person> people = PersonDAO.getList();
+		List<Person> people = personDao.getAll();
 		return people.stream().filter(e -> e.getGender() == 'F')
 				.collect(Collectors.toList());
 	}
